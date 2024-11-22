03479b31663bfd57a0e1ad4f7ea303ab87dbaec6
samarali <engsamar105@gmail.com>
2024-11-23 01:57:19 +0200

    Fullstack Quizzes and Announcements System>

--- client/src/interfaces/AddDataDialogProps.ts
+++
@@ -1,6 +0,0 @@
-export interface AddDataDialogProps {
-  open: boolean;
-  handleClose: () => void;
-  setRender: React.Dispatch<React.SetStateAction<boolean>>;
-  ID?: string;
-}
--- client/src/interfaces/UseAddQuizDialogProps.tsx
+++
@@ -1,4 +0,0 @@
-export interface UseAddQuizDialogProps {
-  handleClose: () => void;
-  setRender: React.Dispatch<React.SetStateAction<boolean>>;
-}
