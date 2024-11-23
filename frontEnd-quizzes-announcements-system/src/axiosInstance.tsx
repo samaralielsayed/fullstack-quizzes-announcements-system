6c63911bdb5ab894daa215f96a670d4564f99d93
samarali <engsamar105@gmail.com>
2024-11-23 17:32:21 +0200

    update

--- frontEnd-quizzes-announcements-system/src/axiosInstance.tsx
+++ frontEnd-quizzes-announcements-system/src/axiosInstance.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://quizzes-announcements-system.vercel.app/api/v1",
 });
 
 export default axiosInstance;
